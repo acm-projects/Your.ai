261297ce89569fa05a11cf1156831b959263e5d6
srana3 <suhanirana19@gmail.com>
2025-03-17 11:14:15 -0500

    fix RAG files issue

---
+++ app.py
@@ -0,0 +1,12 @@
+from langchain_deepseek import ChatDeepSeek
+
+llm = ChatDeepSeek(
+    model="deepseek-chat",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
+
+llm.invoke("Hello, world!")
